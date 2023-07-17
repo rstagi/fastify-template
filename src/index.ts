59cc64ec5af14351174f932b8296041c9ca76f90
rstagi <r.stagi96@gmail.com>
2023-07-18 00:08:47 +0200

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import autoLoad from '@fastify/autoload'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
+
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
 import fastify from 'fastify'
@@ -7,6 +10,11 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = fastify({ logger: { level: process.env.LOG_LEVEL || 'info' } })
+
+await app.register(fastifySwagger)
+await app.register(fastifySwaggerUI, {
+  routePrefix: '/documentation',
+})
 
 app.register(autoLoad, {
   dir: join(__dirname, 'plugins')
