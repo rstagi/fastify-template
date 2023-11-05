8ea4f1bd2fb98089106a789beb2dd3f20cde4528
rstagi <r.stagi96@gmail.com>
2023-11-06 00:54:05 +0100

    fix: improve comments and corner cases

--- builder/build-service.ts
+++ builder/build-service.ts
@@ -1,6 +1,6 @@
-import fastify, { FastifyInstance } from 'fastify';
-import fastifyEnv from '@fastify/env';
-import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+import fastify, { FastifyInstance } from "fastify";
+import fastifyEnv from "@fastify/env";
+import { FromSchema, JSONSchema } from "json-schema-to-ts";
 
 type DecoratedFastify<T> = FastifyInstance & {
   config: T;
@@ -20,7 +20,7 @@ export default function build<T extends JSONSchema | undefined>(
 
     await buildService(app);
 
-    app.listen({ host: '0.0.0.0', port: app.config.PORT }, (err) => {
+    app.listen({ host: "0.0.0.0", port: app.config.PORT || 3000 }, (err) => {
       if (err) {
         app.log.fatal(err);
         process.exit(1);
@@ -39,5 +39,5 @@ async function registerEnv<T>(
     data: data ?? process.env,
   });
 
-  return app as unknown as DecoratedFastify<T>;
+  return app as unknown as DecoratedFastify<T>; // FIXME: there should be a better way to do this (assertion function is not working)
 }
